61543e6c60dfdff0985e47c87978ed6850522d4d
Austen Henry <austen.henry@gmail.com>
2020-07-09 08:07:43 -0500

    Made Node project, installed package to help modify last modified time.

--- timestampRenamer.js
+++ timestampRenamer.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const fs = require('fs');
+const utimes = require('@ronomon/utimes');
+
 //joining path of directory 
 const directoryPath = path.join(__dirname, 'Downloads');
 //passsing directoryPath and callback function
@@ -12,14 +14,15 @@ fs.readdir(directoryPath, function (err, files) {
     files.forEach(function (file) {
         // Do whatever you want to do with the file
         // console.log(file);
-        // updateModifiedStat(file);
-        getLastModifiedFromFilename(file);
+        updateModifiedStat(file);
+        // getLastModifiedFromFilename(file);
     });
 });
 
 function updateModifiedStat(filenameWithExtension) {
     const calculatedLastModified = getLastModifiedFromFilename(filenameWithExtension);
-    fs.stat(path.join(directoryPath, filenameWithExtension), (err, stats) => {
+    const filePath = path.join(directoryPath, filenameWithExtension);
+    fs.stat(filePath, (err, stats) => {
         if(err) {
             throw err;
         }
@@ -27,11 +30,19 @@ function updateModifiedStat(filenameWithExtension) {
         // print file last modified date
         console.log(`File Data Last Modified: ${stats.mtime}`);
         // console.log(`File Status Last Modified: ${stats.ctime}`);
+
+        // Birth time (created time) ?
+        var btime = 447775200000; // 1984-03-10T14:00:00.000Z
+        // Last Modified Time (this is what I need to get in milliseconds from calculatedLastModified)
+        var mtime = undefined; 
+        // Last Accessed Time
+        var atime = undefined;
+        utimes.utimes(filePath, btime, mtime, atime, callback);
     });
 }
 
 function getLastModifiedFromFilename(filename) {
-    // Need to get filename 2020-05-09_12.08.54.mp4 to iso string: 2015-03-25T12:00:00-06:30
+    // Need to get filename 2020-05-09_12.08.54.mp4 to iso string: 2015-03-25T12:00:00Z (or '-06:30' instead of Z)
     const withNoExtension = path.parse(filename).name;
     const withT = withNoExtension.replace('_', 'T');
     const withColons = withT.replace(/\./g, ':');
