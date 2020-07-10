8b6e24934e2c308313f8ef33c82a7250a34780a6
Austen Henry <austen.henry@gmail.com>
2020-07-09 19:12:07 -0500

    Works!

--- timestampRenamer.js
+++ timestampRenamer.js
@@ -2,60 +2,48 @@ const path = require('path');
 const fs = require('fs');
 const utimes = require('@ronomon/utimes');
 
-//joining path of directory 
+// Path of Downloads folder.
 const directoryPath = path.join(__dirname, 'Downloads');
-//passsing directoryPath and callback function
 fs.readdir(directoryPath, function (err, files) {
-    //handling error
     if (err) {
         return console.log('Unable to scan directory: ' + err);
     } 
-    //listing all files using forEach
+
     files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        // console.log(file);
-        updateModifiedStat(file);
-        // getLastModifiedFromFilename(file);
+        const extension = path.extname(file);
+        // Snapchat only returns these two extensions. Filters out .DS_STORE and other junk.
+        if (extension == '.mp4' || extension == '.jpg') {
+            updateTimestamps(file);
+        }
     });
 });
 
-function updateModifiedStat(filenameWithExtension) {
-    const calculatedLastModified = getLastModifiedFromFilename(filenameWithExtension);
+/**
+ * Updates the timestamps (Date Created, Date Modified, Date Accessed) to the original timestamp of when it was taken.
+ * @param {String} filenameWithExtension name of file to update time stamps of.
+ */
+function updateTimestamps(filenameWithExtension) {
+    const calculatedTimestamp = getTimestampFromFilename(filenameWithExtension);
     const filePath = path.join(directoryPath, filenameWithExtension);
-    fs.stat(filePath, (err, stats) => {
-        if(err) {
-            throw err;
-        }
 
-        // print file last modified date
-        console.log(`File Data Last Modified: ${stats.mtime}`);
-        // console.log(`File Status Last Modified: ${stats.ctime}`);
-
-        // Birth time (created time) ?
-        var btime = 447775200000; // 1984-03-10T14:00:00.000Z
-        // Last Modified Time (this is what I need to get in milliseconds from calculatedLastModified)
-        var mtime = undefined; 
-        // Last Accessed Time
-        var atime = undefined;
-        utimes.utimes(filePath, btime, mtime, atime, callback);
-    });
+    // Last Modified Time (this is what I need to get in milliseconds from calculatedLastModified)
+    var mtime = Date.parse(calculatedTimestamp);
+    // Could be NaN. Probably better ways to handle this but I am only human.
+    if (mtime) {
+        utimes.utimes(filePath, mtime, mtime, mtime, () => {
+            console.log('Done updating ' + filenameWithExtension);
+        });
+    }
 }
 
-function getLastModifiedFromFilename(filename) {
+/**
+ * Converts the timestamp in the file name to a UTC ISO8601 Timestamp.
+ * @param {String} filename the name of the file (in our case it's something like '2020-05-09_12.08.54.mp4').
+ */
+function getTimestampFromFilename(filename) {
     // Need to get filename 2020-05-09_12.08.54.mp4 to iso string: 2015-03-25T12:00:00Z (or '-06:30' instead of Z)
     const withNoExtension = path.parse(filename).name;
     const withT = withNoExtension.replace('_', 'T');
     const withColons = withT.replace(/\./g, ':');
     return withColons + 'Z';
 }
-
-
-
-// Need this script to read in all snapchat files, read in the timestamp in the filename, and then modify
-// the date last modified to be the date and time of the filename. UGH
-/**
- * https://medium.com/stackfame/get-list-of-all-files-in-a-directory-in-node-js-befd31677ec5
- * https://attacomsian.com/blog/reading-writing-files-nodejs
- * https://attacomsian.com/blog/nodejs-get-file-last-modified-date#:~:text=about%20a%20file.-,The%20fs.,status%20was%20changed%20last%20time.
- * 
- */
